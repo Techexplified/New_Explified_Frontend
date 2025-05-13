4f172ff5575a6b54dc1f7799dfcc1e3f5cbf8c94
vishvjeetprajapati <vishvjeetprajapati138@gmail.com>
2025-05-13 22:13:31 +0530

    added gif tool

--- src/network/axiosInstance.js
+++ src/network/axiosInstance.js
@@ -4,4 +4,5 @@ const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_URL,
 });
 
+
 export default axiosInstance;
--- src/pages/explified_tools/background_remover/BackgroundRemover.jsx
+++ src/pages/explified_tools/background_remover/BackgroundRemover.jsx
@@ -54,6 +54,8 @@ const RemoveBackground = () => {
     }
   };
 
+
+  
   console.log(imageBg);
 
   return (
