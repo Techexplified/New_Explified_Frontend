00067c18a34ab366278547c6c238dade732ffa37
saritasubhra <saritasubhra1@gmail.com>
2025-05-29 20:05:35 +0530

    routing changed 2

--- src/pages/extensions/NewDashboard.jsx
+++ src/pages/extensions/NewDashboard.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 function NewDashboard() {
+  const navigate = useNavigate();
+
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
   return (
     <div>
       <h1 className="text-center text-4xl">Welcome to your dashboard</h1>
