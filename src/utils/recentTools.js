775588e080c5db52dee1d91daea9cb05511df184
Mradul <=>
2025-08-22 22:29:09 +0530

    added recents and search features

---
+++ src/utils/recentTools.js
@@ -0,0 +1,21 @@
+// utils/recentTools.js
+export const addRecentTool = (tool) => {
+  let recent = JSON.parse(localStorage.getItem("recentTools")) || [];
+
+  // Remove if already exists
+  recent = recent.filter((t) => t.title !== tool.title);
+
+  // Add latest
+  recent.push(tool);
+
+  // Limit to 6 tools
+  if (recent.length > 6) {
+    recent = recent.slice(recent.length - 6);
+  }
+
+  localStorage.setItem("recentTools", JSON.stringify(recent));
+};
+
+export const getRecentTools = () => {
+  return JSON.parse(localStorage.getItem("recentTools")) || [];
+};
