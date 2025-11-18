37d95064c3ee22ffafd361ce724c594c711ecfbb
saritasubhra <saritasubhra1@gmail.com>
2025-11-18 22:50:50 +0530

    sailaja's code added

---
+++ public/bg-remover.js
@@ -0,0 +1,63 @@
+export const backgroundThumbnails = [
+  "./images/background/bg1.jpg",
+  "./images/background/bg2.jpg",
+  "./images/background/bg3.jpg",
+  "./images/background/bg4.jpg",
+  "./images/background/bg5.jpg",
+  "./images/background/bg6.jpg",
+  "./images/background/bg7.jpg",
+  "./images/background/bg8.jpg",
+  "./images/background/bg9.jpg",
+  "./images/background/bg10.jpg",
+  "./images/background/bg11.jpg",
+  "./images/background/bg12.jpg",
+];
+
+export const imageThumbnails = [
+  "./images/background/img1.jpg",
+  "./images/background/img2.jpg",
+  "./images/background/img3.jpg",
+  "./images/background/img4.jpg",
+  "./images/background/img5.jpg",
+  "./images/background/img6.jpg",
+  "./images/background/img7.jpg",
+  "./images/background/img8.jpg",
+  "./images/background/img9.jpg",
+  "./images/background/img10.jpg",
+  "./images/background/img11.jpg",
+];
+
+export const colorOptions = [
+  "#ffffff",
+  "#000000",
+  "#ff5757",
+  "#ffc947",
+  "#27ae60",
+  "#3498db",
+  "#9b59b6",
+  "#f368e0",
+  "#ff9f43",
+  "#10ac84",
+  // Pastels
+  "#ffccd5",
+  "#ffd6a5",
+  "#fdffb6",
+  "#caffbf",
+  "#9bf6ff",
+  "#a0c4ff",
+  "#bdb2ff",
+  "#ffc6ff",
+  // Earthy tones
+  "#6d6875",
+  "#b5838d",
+  "#e5989b",
+  "#ffb4a2",
+  "#ffcdb2",
+
+  // Premium / luxury tones
+  "#b29c67",
+  "#6a4e23",
+  "#3c2f2f",
+  "#4a3f35",
+  "#7a6f60",
+];
